c04e9c5b1336c16dd882df0409466b2a19b10576
anil <chhetrianilartory@gmail.com>
2024-08-30 17:52:19 +0900

    d

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,10 +28,10 @@ export const signin = async(req,res,next) =>{
            
             const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET)
             
-            //this is for security method not sending pass destroy the password after login
+            //this is for security method not sending pass destroy the password after login test
             const {password: pass, ...rest} = validUser._doc;
             res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
         } catch (error) {
             next(error)
         }
-} 
+}  
